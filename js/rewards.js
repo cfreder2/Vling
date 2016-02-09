4c28a345884a7a2ae0898411e24d816cfa654699
Chris Frederick <cfreder2@cfreder2-mbpro-2014.local>
2016-02-08 23:54:08 -0500

    Initial Parse Integration.  Still using it...

---
+++ js/rewards.js
@@ -0,0 +1,75 @@
+Parse.$ = jQuery;
+
+
+Parse.initialize('sc0hPlJJJZDZEhnRAD0vUn8EIT8ZqkXQGm407Yui',
+'6faVIwMvo7zbcWnR0Bj014g0Pz7Iii5xQIU0tYyG'); // APPKEY, JAVASCRIPTKEY
+
+
+(function(){
+
+  $(document).ready(init);
+
+  function init() {
+    loadPoints();
+    loadRewards();
+  }
+
+  function loadPoints() {
+    var query = new Parse.Query(Parse.User);
+    query.equalTo("username","cfreder2");
+    
+    query.find({
+      success: function(results){
+        for (var i = 0; i < results.length; i++) {
+          var object = results[i];
+          var points = object.get('points');
+          
+          var html =  '<h3>'+ points +'pts</h3>';
+          $('#pts').append(html);
+        }
+      },
+      error: function(error) { alert('Error: ' + error.code + ' ' + error.message); }
+    });
+
+  }
+
+  function loadRewards() {
+
+    var reward = Parse.Object.extend('reward');
+    var query = new Parse.Query(reward);
+    
+    query.find({
+      success: function(results){
+        for (var i = 0; i < results.length; i++) {
+          var object = results[i];
+          var title = object.get('title');
+          var description = object.get('description');
+          var cost = object.get('cost');
+          var imageURL = object.get("image").url();
+          var section_class = 'a';
+          if(i % 2) {
+            section_class = 'b';
+          }
+          
+          var html =  '<div class="content-section-'+section_class + '">' +
+                      '<div class="container">' +
+                      '<div class="row">' +
+                      '<div class="col-lg-5 col-sm-6">' +
+                      '<div class="clearfix"></div>' +
+                      '<h2 class="section-heading">'+ object.get('title') +'</h2>' +
+                      '<p class="lead">' + object.get('description') + '</p>' +
+                      '<a href="./rewards.html" class="btn btn-default btn-lg"> <span class="network-name">Redeem for ' + cost + 'pts</span></a>' +
+                      '</div>' +
+                      '<div class="col-lg-5 col-lg-offset-2 col-sm-6">' +
+                      '<img class="img-responsive" src="' + imageURL + '" alt="">' +
+                      '</div></div></div></div>';
+          $('#rewards').append(html);
+        }
+      },
+      error: function(error) { alert('Error: ' + error.code + ' ' + error.message); }
+    });
+
+  }
+
+
+})();
